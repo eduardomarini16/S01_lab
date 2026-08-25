2fd18f28f118e8b1be7be6f4f211a5d3f03a18b1
@eduardomarini <eduardo.marini@ativasolucoes.com.br>
2026-08-24 21:03:15 -0300

    ADD: exercicio1 relatorio02

---
+++ relatorio02/exercicio1.lua
@@ -0,0 +1,22 @@
+print ("Exercicio 1")
+
+function gerarTabelasPotenciais(inicio, fim, base)
+    for expoente = inicio, fim do
+        local op = base ^ expoente
+        print(base .. " ^ " .. expoente .. " = " .. op)
+    end
+end
+
+print("Digite o expoente inicial: ")
+local M = tonumber(io.read())
+print("Digite o expoente final: ")
+local N = tonumber(io.read())
+print("Digite a base: ")
+local B = tonumber(io.read())
+
+if m <= n then
+    gerarTabelasPotenciais(M,N,B)
+else 
+    print("Erro, expoente inicial deve ser menor ou igual a expoente final.")
+end
+
