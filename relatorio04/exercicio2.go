0005cac6ee2a81cae98fded67db589f3563e11b6
@eduardomarini <eduardo.marini@ativasolucoes.com.br>
2026-09-09 09:49:37 -0300

    FIX: comentar exer2/exer3 para teste exercicio 4

--- relatorio04/exercicio2.go
+++ relatorio04/exercicio2.go
@@ -1,35 +1,35 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func somaValoresVendas(a, b, c int) int {
-	return a + b + c
-}
+// func somaValoresVendas(a, b, c int) int {
+// 	return a + b + c
+// }
 
-func main() {
+// func main() {
 
-	var a, b, c int
+// 	var a, b, c int
 
-	fmt.Println("Digite as vendas do 1° trimestre: ")
-	fmt.Scan(&a)
-	fmt.Println("Digite as vendas do 2° trimestre: ")
-	fmt.Scan(&b)
-	fmt.Println("Digite as vendas do 3° trimestre: ")
-	fmt.Scan(&c)
+// 	fmt.Println("Digite as vendas do 1° trimestre: ")
+// 	fmt.Scan(&a)
+// 	fmt.Println("Digite as vendas do 2° trimestre: ")
+// 	fmt.Scan(&b)
+// 	fmt.Println("Digite as vendas do 3° trimestre: ")
+// 	fmt.Scan(&c)
 
-	soma_vendas := somaValoresVendas(a, b, c)
+// 	soma_vendas := somaValoresVendas(a, b, c)
 
-	if soma_vendas < 100 {
-		fmt.Println("Meta mínima anual não atingida!")
-	} else {
-		fmt.Printf("Total de vendas: %d unidades\n", soma_vendas)
-		switch {
-		case soma_vendas >= 250:
-			fmt.Println("Classificação: Categoria Top Seller")
-		case soma_vendas >= 180 && soma_vendas < 250:
-			fmt.Println("Classificação: Categoria Sênior")
-		case soma_vendas >= 100 && soma_vendas < 180:
-			fmt.Println("Classificação: Categoria Pleno")
-		}
-	}
-}
+// 	if soma_vendas < 100 {
+// 		fmt.Println("Meta mínima anual não atingida!")
+// 	} else {
+// 		fmt.Printf("Total de vendas: %d unidades\n", soma_vendas)
+// 		switch {
+// 		case soma_vendas >= 250:
+// 			fmt.Println("Classificação: Categoria Top Seller")
+// 		case soma_vendas >= 180 && soma_vendas < 250:
+// 			fmt.Println("Classificação: Categoria Sênior")
+// 		case soma_vendas >= 100 && soma_vendas < 180:
+// 			fmt.Println("Classificação: Categoria Pleno")
+// 		}
+// 	}
+// }
--- relatorio04/exercicio3.go
+++ relatorio04/exercicio3.go
@@ -1,20 +1,20 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func gerarEscalaPlantao(n int) {
-	dia := 1
-	for i := 1; i <= n; i++ {
-		fmt.Printf("Plantão %d: Dia %d do mês\n", i, dia)
-		dia += 4
-	}
-}
+// func gerarEscalaPlantao(n int) {
+// 	dia := 1
+// 	for i := 1; i <= n; i++ {
+// 		fmt.Printf("Plantão %d: Dia %d do mês\n", i, dia)
+// 		dia += 4
+// 	}
+// }
 
-func main() {
-	var n int
+// func main() {
+// 	var n int
 
-	fmt.Print("Digite a quantidade de plantões: ")
-	fmt.Scan(&n)
+// 	fmt.Print("Digite a quantidade de plantões: ")
+// 	fmt.Scan(&n)
 
-	gerarEscalaPlantao(n)
-}
+// 	gerarEscalaPlantao(n)
+// }
