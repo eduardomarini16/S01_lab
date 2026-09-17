a3d6a236ec17879990c298c842e94da59083717c
@eduardomarini <eduardo.marini@ativasolucoes.com.br>
2026-09-17 09:35:05 -0300

    ADD: folder relatorio05 / resolução exercicio1 C++

---
+++ relatorio05/exercicio1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int combinar_equipes(int n) {
+    if (n == 0) {
+        return 0;
+    }
+
+    if (n == 1) {
+        return 1;
+    }
+
+    return combinar_equipes(n - 1) + combinar_equipes(n - 2);
+}
+
+int main() {
+    int n;
+
+    cout << "Digite o tamanho do chaveamento: ";
+    if (!(cin >> n) || n < 0) {
+        cout << "Digite um numero inteiro nao negativo." << endl;
+        return 1;
+    }
+
+    cout << "Total de combinacoes: " << combinar_equipes(n) << endl;
+
+    return 0;
+}
